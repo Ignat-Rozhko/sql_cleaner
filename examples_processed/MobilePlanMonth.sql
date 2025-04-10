19208688d7e418314d6cbf7c08e26009bd9f5309
Ignat Rozhko <ignatrozhko@gmail.com>
2025-04-10 11:49:52 +0500

    Remove obsolete SQL files: AgentSettingService.sql, MobilePlanMonth.sql, and multi_value_example.sql. Update PriceChangeServiceTest.sql to include new insert statements and correct syntax errors, enhancing the test data setup.

--- examples_processed/MobilePlanMonth.sql
+++
@@ -1,9 +0,0 @@
-set session_replication_role = replica;
-
-insert into public.employee (id, name, tenant_id, version, created_by, created_date, last_modified_by,
-                             last_modified_date, first_name, middle_name, last_name, position_id, code,
-                             team_id, recycle_bin_id)
-values ('15cc0e3a-8921-1589-f7a4-c6fb6df38e66', 'иванов', 'acc', 2, '01', '2023-05-15 11:25:01.252000', '01',
-        '2023-05-19 12:04:33.013000', 'иван', 'иванович', 'иванов', null, '0000000001',
-        null, null);
-
--- examples_processed/multi_value_example.sql
+++
@@ -1,4 +0,0 @@
-
-    
-INSERT INTO product (id, name, version) VALUES ('dd76c9f6-7da6-4622-818b-b1d837fc8142', 'Product 1', 1), ('ee76c9f6-7da6-4622-818b-b1d837fc9253', 'Product 2', 1);
-    
